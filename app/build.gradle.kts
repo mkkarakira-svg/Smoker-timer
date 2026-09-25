e994dc25047786498618f26eff62ae8200c36608
mkkarakira-svg <mk.karakira@gmail.com>
2026-09-25 10:07:24 +0300

    Build Smoker Timer v1.0

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+ namespace = "com.smokertimer.app"; compileSdk = 35
+ defaultConfig { applicationId = "com.smokertimer.app"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+}
